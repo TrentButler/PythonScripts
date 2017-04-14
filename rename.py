6e570635c157edf1e8280e30b0abb36f6f7b005f
Trent Butler <redtrentbutler@gmail.com>
2017-04-13 21:50:37 -0500

    Rename 04-13-17

--- rename.py
+++ rename.py
@@ -1,13 +1,27 @@
 import os
- 
- current_dir = os.listdir(os.getcwd())
- _input = raw_input('DESIRED NAME: ')
- counter = 0
- 
- for thing in current_dir:
-    if thing.endswith('.py'):
-         continue
-     extension = os.path.splitext(thing)[1]
-     os.rename(thing, _input + "_" + str(counter) +  extension)
-     counter += 1   
-print 'SUCCESS' 
+
+current_dir = os.listdir(os.getcwd())
+get_extension = raw_input('enter file extension to rename ')
+second_input = raw_input('1. replace entire name\n2. prepend name ')
+desired_name = raw_input('enter desired name ')
+counter = 0
+
+if second_input == '1':    
+    for thing in current_dir:
+        if thing.endswith('.py'):
+            continue
+        if thing.endswith(get_extension) is False:
+            continue
+        extension = os.path.splitext(thing)[1]
+        os.rename(thing, desired_name + "_" + str(counter) +  extension)
+        counter += 1
+
+if second_input is '2':
+    for thing in current_dir:
+        if thing.endswith('.py'):
+            continue
+        if thing.endswith(get_extension) is False:
+            continue
+        fname = os.path.splitext(thing)[0]
+        extension = os.path.splitext(thing)[1]        
+        os.rename(thing, desired_name + fname + extension)     
